13ee7ca2ca24a96e5c43edd07b44fa638f0ab718
ahmad fasya <ahmadfasya@gmail.com>
2019-08-26 05:45:46 +0700

    services

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /* ========================================================
                         preloader
 =========================================================== */
-$(window).on('load', function(){
-    $("#status").fadeOut(3000);
+$(window).on('load', function () {
+    $("#status").fadeOut(2000);
     $("#preloader").delay(1000).fadeOut('slow');
 
 })
@@ -13,7 +13,7 @@ $(window).on('load', function(){
 
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-        items:2,
+        items: 2,
         autoplay: true,
         smartSpeed: 700,
         loop: true,
@@ -23,3 +23,33 @@ $(document).ready(function () {
         // navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
     });
 });
+
+
+/* ========================================================
+                        progress bar
+=========================================================== */
+$(function () {
+    $("#progress-elements").waypoint(function () {
+        $(".progress-bar").each(function () {
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 3000);
+        });
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });
+
+
+});
+
+
+/* ========================================================
+                        progress bar
+=========================================================== */
+$(function() {
+    $("#services-tabs").responsiveTabs({
+        // source : https://github.com/jellekralt/Responsive-Tabs
+        animation: 'slide', // The panels will slide up and down
+    });
+})
